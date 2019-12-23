98940297f16829e2c9af336cfcdf3803d1c29502
CoderShiun <123657@gmail.com>
2019-12-23 16:47:03 +0100

    improve 1281

--- 1281_subtract_the_product_and_sum_of_digits_of_an_integer/1281.go
+++ 1281_subtract_the_product_and_sum_of_digits_of_an_integer/1281.go
@@ -1,39 +1,18 @@
 package subtract_the_product_and_sum_of_digits_of_an_integer
 
 import (
-	"fmt"
 	"strconv"
 )
 
 func SubtractProductAndSum(n int) int {
-	product := product(n)
-	sum := sum(n)
+	var product = 1
+	var sum int
+
+	for _, v := range strconv.Itoa(n) {
+		num, _ := strconv.Atoi(string(v))
+		product *= num
+		sum += num
+	}
 
 	return product - sum
-}
-
-func product (n int) int {
-	var result = 1
-
-	for _, v := range strconv.Itoa(n) {
-
-		num, _ := strconv.Atoi(string(v))
-		result *= num
-	}
-
-
-	return result
-}
-
-func sum (n int) int {
-	var result int
-
-	for _, v := range strconv.Itoa(n) {
-		num, _ := strconv.Atoi(string(v))
-		result += num
-	}
-
-	fmt.Println(result)
-
-	return result
 }
